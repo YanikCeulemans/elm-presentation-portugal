ed36778d3ceb06159a6904731baddae9263f0a53
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2016-09-19 22:20:14 +0200

    Finished interactive hello world

--- Main.elm
+++ Main.elm
@@ -1,7 +1,52 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (Html)
+import Html.App
+import Html.Events
+
+
+type Msg
+    = SayHelloToEuricom
+    | SayHelloToElm
+
+
+type alias Model =
+    String
+
+
+model : Model
+model =
+    "world"
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SayHelloToEuricom ->
+            "Euricom"
+
+        SayHelloToElm ->
+            "Elm"
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ Html.button
+            [ Html.Events.onClick SayHelloToEuricom
+            ]
+            [ Html.text "Say hello to Euricom" ]
+        , Html.button
+            [ Html.Events.onClick SayHelloToElm
+            ]
+            [ Html.text "Say hello to Elm" ]
+        , Html.div [] [ Html.text ("Hello " ++ model ++ "!") ]
+        ]
 
 
 main =
-    Html.text "Hello world"
+    Html.App.beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
